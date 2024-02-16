1ee712809d466c72390dbedcb8bb08cdceac4f2b
jrr6 <7482866+jrr6@users.noreply.github.com>
2024-02-16 10:34:50 -0500

    Update have tactic

--- BrownCs22/Library/Tactic/IntroRules.lean
+++ BrownCs22/Library/Tactic/IntroRules.lean
@@ -14,24 +14,33 @@ elab (name := split_goal) "split_goal " : tactic => withMainContext do
 
 macro "reflexivity" : tactic => `(tactic |rfl)
 
-macro "have " d:haveDecl : tactic =>
+syntax (name := haveCS22) "have " haveDecl : tactic
+@[tactic haveCS22] def elabHaveCS22 : Tactic
+| `(haveCS22| have $d:haveDecl) => withMainContext do
   match d.raw.getHead? with
     | none =>
       -- This case should be unreachable, but I'm not confident enough in that
       -- assertion to actually put an `unreachable!` here and risk confusing
       -- students with a server panic in some weird edge case
-      `(tactic| fail "Invalid syntax provided to `have`.")
+      throwError "Invalid syntax provided to `have`."
     | some nmStx =>
       let nm := nmStx.getId
+      let willShadow := (← getLCtx).decls.any λ
+        | none => false
+        | some d => !d.isImplementationDetail && d.userName == nm
       if nm.isAnonymous then
-        let errStr := "Your `have` declaration is missing a name. Make sure to "
-            ++ "specify a name for your new hypothesis before the colon."
-        `(tactic| fail $(Syntax.mkStrLit errStr))
+        throwError ("Your `have` declaration is missing a name. Make sure to "
+                    ++ "specify a name for your new hypothesis before the colon.")
       else if !(isValidHypName nm.toString) then
-        let errStr := "Names of hypotheses should begin with \"h\", but the "
-            ++ s!"name \"{nm.toString}\" does not. If you're trying to declare "
-            ++ "a piece of data instead, remember that `have` is only for "
-            ++ "adding hypotheses to your context, not data."
-        `(tactic| fail $(Syntax.mkStrLit errStr))
+        throwError ("Names of hypotheses should begin with \"h\", but the "
+                      ++ s!"name \"{nm.toString}\" does not. If you're trying to declare "
+                      ++ "a piece of data instead, remember that `have` is only for "
+                      ++ "adding hypotheses to your context, not data.")
+      else if willShadow then
+        throwError s!"There is already a hypothesis in your context named \"{nm}\""
       else
-        `(tactic| refine_lift have $d:haveDecl; ?_)
+          evalTactic (← `(tactic| refine_lift have $d:haveDecl; ?_ ))
+            <|> throwError ("Your `have` declaration could not be processed. Most likely, this "
+                ++ "means that the expression to the right of the `:=` sign is not syntactically "
+                ++ "valid.")
+| _ => throwUnsupportedSyntax
